525fb663ea5b97bc7a4755f490630254bc747a15
anass <aessamadi@edu.tecnocampus.cat>
2024-07-03 23:15:09 +0200

    Commit 28

--- moduleBowling/bowling.js
+++ moduleBowling/bowling.js
@@ -7,10 +7,15 @@ class Game{
     score(){
         const rolls=this._rolls;
         let score=0;
-        let i=0;
+        let frameIndex=0;
         for(let frame=0;frame<10;frame++){
-            score+=this._rolls[i]+this._rolls[i+1];
-            i+=2;
+            if(rolls[frameIndex]+rolls[frameIndex+1]==10){
+                score+=10+rolls[frameIndex+2];
+                frameIndex+=2;
+            }else{
+                score+=rolls[frameIndex]+rolls[frameIndex+1];
+                frameIndex+=2;
+            }
         }
         return score;
     }
