bfa6d20da7910ceb887bd7bbf319add5d3b19336
anass <aessamadi@edu.tecnocampus.cat>
2024-07-03 23:00:30 +0200

    Commit 8

--- moduleBowling/bowling.spec.js
+++ moduleBowling/bowling.spec.js
@@ -1,6 +1,6 @@
 const {Game}= require('./bowling');
 
-test('gutter game', () => {
+test("gutter game", () => {
     const g = new Game();
     for(let i=0;i<20;i++){
         g.roll(0);
@@ -8,3 +8,10 @@ test('gutter game', () => {
     expect(g.score()).toBe(0);
 });
 
+test("all ones", () => {
+    const g = new Game();
+    for(let i=0;i<20;i++){
+        g.roll(1);
+    }
+    expect(g.score()).toBe(20);
+})
